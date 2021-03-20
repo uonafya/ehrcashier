f56182b4077bb73829f3049c44552f23c0ab168e
ningosi <nicotwendelee@gmail.com>
2021-03-20 20:54:51 +0300

    Add the security model to the cashier application

---
+++ api/src/main/java/org/openmrs/module/ehrcashier/metadata/EhrCashierSecurityMetadata.java
@@ -0,0 +1,41 @@
+package org.openmrs.module.ehrcashier.metadata;
+
+import org.openmrs.module.metadatadeploy.bundle.Requires;
+import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
+import org.springframework.stereotype.Component;
+
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.idSet;
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.privilege;
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.role;
+
+@Component
+@Requires(org.openmrs.module.kenyaemr.metadata.SecurityMetadata.class)
+public class EhrCashierSecurityMetadata extends AbstractMetadataBundle {
+	
+	/**
+	 * Application IDs
+	 */
+	
+	public static final class _Privilege {
+		
+		public static final String CASHIER_MODULE_APP = "App: ehrcashier.billing";
+	}
+	
+	public static final class _Role {
+		
+		public static final String CASHIER = "Cashier";
+		
+	}
+	
+	/**
+	 * @see AbstractMetadataBundle#install()
+	 */
+	@Override
+	public void install() {
+		install(privilege(_Privilege.CASHIER_MODULE_APP, "Able to access EHR Cashier module features"));
+		
+		install(role(_Role.CASHIER, "Can access Key EHR Cashier module App",
+		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
+		    idSet(_Privilege.CASHIER_MODULE_APP)));
+	}
+}
--- omod/src/main/java/org/openmrs/module/ehrcashier/page/controller/BillingQueuePageController.java
+++ omod/src/main/java/org/openmrs/module/ehrcashier/page/controller/BillingQueuePageController.java
@@ -26,11 +26,6 @@ import java.util.Date;
 public class BillingQueuePageController {
 	
 	public String get(PageModel pageModel, UiSessionContext sessionContext, PageRequest pageRequest, UiUtils ui) {
-		BillAccess ba = new BillAccess();
-		boolean auth = ba.authenticate(pageRequest, sessionContext, ui);
-		if (!auth) {
-			return "redirect: index.htm";
-		}
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		String dateStr = sdf.format(new Date());
 		pageModel.addAttribute("currentDate", dateStr);
